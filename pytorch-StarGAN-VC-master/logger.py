be31b3d24c85332f79745f2bffcc1b3ff06204e1
Liang-Yu Lu <liang-yulu@Liang-Yus-MacBook-Pro.local>
2020-08-25 13:31:12 +0800

    debug_new

--- pytorch-StarGAN-VC-master/logger.py
+++ pytorch-StarGAN-VC-master/logger.py
@@ -13,6 +13,7 @@ class Logger(object):
          with self.writer.as_default():
             tf.summary.scalar(tag, value, step=step)
             self.writer.flush()
+    '''
     def list_of_scalars_summary(self, tag_value_pairs, step):
         """Log scalar variables."""
         summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value) for tag, value in tag_value_pairs])
@@ -23,3 +24,5 @@ class Logger(object):
             self.writer.flush()
         # summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value) for tag, value in tag_value_pairs])
         # self.writer.add_summary(summary, step)
+    '''
+    
